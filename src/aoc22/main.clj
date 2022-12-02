1a4f4a1c94be3dd1a251a88c6dfeff110d35e327
Mikko Seppälä <mikko.seppala@gofore.com>
2022-12-02 18:59:50 +0200

    refactor main, template for day 3

--- src/aoc22/main.clj
+++ src/aoc22/main.clj
@@ -1,10 +1,10 @@
 (ns aoc22.main 
-  (:require [aoc22.day1 :as day1]
-            [aoc22.day2 :as day2]))
+  (:require [aoc22.day1]
+            [aoc22.day2]
+            [aoc22.day3]))
 
 (defn -main [which]
-  (let [day (Integer. which)]
-    (case day
-      1 (println "Exercise 1:" (day1/-main))
-      2 (println "Exercise 2:" (day2/-main)) 
-      )))
+  (let [day (Integer. which)
+        puzzle-fn (format "aoc22.day%s/-main" day)
+        result (apply (resolve (symbol puzzle-fn)) [])]
+    (println (format "Puzzle %s: %s" day result))))
