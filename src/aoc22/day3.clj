2fd5c1f2c5ef2e3128d1cf16c8e2c4dc0d8ba3eb
Mikko Seppälä <mikko.seppala@gofore.com>
2022-12-03 17:54:16 +0200

    day3 part2

--- src/aoc22/day3.clj
+++ src/aoc22/day3.clj
@@ -8,18 +8,15 @@
     (+ 27 (- (int item) (int \A)))
     (+ 1  (- (int item) (int \a)))))
 
+(defn common-item [rucksack-group]
+  (first (apply clojure.set/intersection 
+                (map set rucksack-group))))
 
-(defn common-item [rucksack]
-  (first
-   (apply clojure.set/intersection
-          (map set
-               (split-at
-                (/ (count rucksack) 2) rucksack)))))
-
-(defn -main [] (->>
-                (slurp "resources/day3.txt")
-                (str/split-lines)
-                (map common-item)
-                (map item-value)
-                (reduce +)))
-
+(defn -main []
+  (->>
+   (slurp "resources/day3.txt")
+   (str/split-lines)
+   (partition 3)
+   (map common-item)
+   (map item-value)
+   (reduce +)))
