0bfcac3421cac6ffa0e8cb0b76457ccf989e9620
Mikko Seppälä <mikko.seppala@gofore.com>
2022-12-04 14:10:01 +0200

    day4 part2

--- src/aoc22/day4.clj
+++ src/aoc22/day4.clj
@@ -4,14 +4,19 @@
 
 (defn full-overlap [assignment]
   (and
-     (<= (get-in assignment [0 0]) (get-in assignment [1 0]))
-     (>= (get-in assignment [0 1]) (get-in assignment[1 1]))))
+   (<= (get-in assignment [0 0]) (get-in assignment [1 0]))
+   (>= (get-in assignment [0 1]) (get-in assignment [1 1]))))
+
+(defn any-overlap [assignment]
+  (not (or
+   (every? #(< % (get-in assignment [0 0])) (assignment 1))
+   (every? #(> % (get-in assignment [0 1])) (assignment 1))))) 
 
 (defn parse-assignment [assignment-string]
   (->>
    (str/split assignment-string #",")
-   (map #(str/split % #"-"))
-   (map #(vec (map read-string %)))
+   (map #(str/split % #"-")) 
+   (map #(->> % (map read-string) (vec)))
    (sort-by #(- (get % 0) (get % 1)))
    (vec)))
 
@@ -20,6 +25,5 @@
    (slurp "resources/day4.txt")
    (str/split-lines)
    (map parse-assignment)
-   (filter full-overlap)
-   (count)
-   ))
+   (filter any-overlap) ;(filter full-overlap) for task1
+   (count)))
