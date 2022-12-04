e3f05f7166b1ab40eeffc296b17e81633ca637e7
Mikko Seppälä <mikko.seppala@gofore.com>
2022-12-04 16:07:24 +0200

    refactor day4 to more readable

--- src/aoc22/day4.clj
+++ src/aoc22/day4.clj
@@ -2,21 +2,22 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(defn numberInRange [number range] 
-  (and (>= number (range 0)) (<= number (range 1))))
+(defn number-in-interval [number interval]
+  (and (>= number (interval 0)) (<= number (interval 1))))
 
-(defn full-overlap [assignment]
-  (every? #(numberInRange % (assignment 0)) (assignment 1)))
+(defn full-overlap? [a b]
+  (or (every? #(number-in-interval % a) b)
+      (every? #(number-in-interval % b) a)))
 
-(defn any-overlap [assignment]
-  (some #(numberInRange % (assignment 0)) (assignment 1)))
+(defn any-overlap? [a b]
+  (or (some #(number-in-interval % a) b)
+      (some #(number-in-interval % b) a)))
 
 (defn parse-assignment [assignment-string]
   (->>
    (str/split assignment-string #",")
-   (map #(str/split % #"-")) 
+   (map #(str/split % #"-"))
    (map #(->> % (map read-string) (vec)))
-   (sort-by #(- (get % 0) (get % 1)))
    (vec)))
 
 (defn -main []
@@ -24,5 +25,5 @@
    (slurp "resources/day4.txt")
    (str/split-lines)
    (map parse-assignment)
-   (filter any-overlap) ;(filter full-overlap) for task1
+   (filter #(apply any-overlap? %)) ;use full-overlap for task1
    (count)))
