6c71f675c92aaf0a6d0fe7125d09309ac652281a
Mikko Seppälä <mikko.seppala@gofore.com>
2022-12-08 22:19:45 +0200

    day8 formatting

--- src/aoc22/day8.clj
+++ src/aoc22/day8.clj
@@ -17,34 +17,31 @@
       (every? #(> (tree-row idx) %) (subvec tree-row (+ 1 idx)))))
 
 (defn tree-visible [x y trees]
-  (if
-   (or
-    (tree-visible-in-row? (trees y) x)
-    (tree-visible-in-row? (column x trees) y))
-    1 0))
+  (if (or (tree-visible-in-row? (trees y) x)
+          (tree-visible-in-row? (column x trees) y))
+    1
+    0))
 
 (defn mark-trees [marker-fn trees]
-  (map-indexed
-   (fn [y row]
-     (map-indexed
-      (fn [x _] (marker-fn x y trees))
-      row))
+  (map-indexed 
+   (fn [y row] (map-indexed 
+                (fn [x _] (marker-fn x y trees)) 
+                row))
    trees))
 
 (defn scenic-score-one-direction [line-of-sight]
   (or (first (keep-indexed
               #(when (<= (first line-of-sight) %2) (inc %1))
-              (rest line-of-sight))) (count (rest line-of-sight))))
+              (rest line-of-sight)))
+      (count (rest line-of-sight))))
 
 (defn scenic-score-row [tree-row idx]
-  (*
-   (scenic-score-one-direction (subvec tree-row idx))
-   (scenic-score-one-direction (reverse (subvec tree-row 0 (inc idx))))))
+  (* (scenic-score-one-direction (subvec tree-row idx))
+     (scenic-score-one-direction (reverse (subvec tree-row 0 (inc idx))))))
 
 (defn scenic-score [x y trees]
-  (*
-   (scenic-score-row (trees y) x)
-   (scenic-score-row (column x trees) y)))
+  (* (scenic-score-row (trees y) x)
+     (scenic-score-row (column x trees) y)))
 
 (defn part1 [tree-map]
   (->> (mark-trees tree-visible tree-map)
@@ -58,4 +55,5 @@
 
 (defn -main []
   (let [tree-map (parse-tree-map)]
-    {:part1 (part1 tree-map) :part2 (part2 tree-map)}))
+    {:part1 (part1 tree-map)
+     :part2 (part2 tree-map)}))
