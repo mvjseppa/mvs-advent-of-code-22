7a0ac860f912e0ec45566a057375db71fb890e72
Mikko Seppälä <mikko.seppala@gofore.com>
2022-12-14 00:33:46 +0200

    day13 replace ifs with cond

--- src/aoc22/day13.clj
+++ src/aoc22/day13.clj
@@ -2,50 +2,45 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(def package-pairs
-  (->> (slurp "resources/day13.txt")
-       (#(str/split % #"\n\n"))
-       (map (fn [pair-str]
-              (->> pair-str
-                   (str/split-lines)
-                   (map read-string))))))
-
-(def package-list
+(def packet-list
   (->> (slurp "resources/day13.txt")
        (str/split-lines)
        (remove str/blank?)
-       (map read-string)
-       (concat [[[2]] [[6]]])))
+       (map read-string)))
+
+(def packet-list-with-markers
+  (concat packet-list [[[2]] [[6]]]))
+
+(def packet-pairs
+  (partition 2 packet-list))
 
 (defn- is-divider? [packet]
   (or (= packet [[2]]) (= packet [[6]])))
 
 (defn- adv-compare-lists [l r cmp-fn]
   (loop [l l r r]
-
-    (if (every? empty? [l r]) 0
-        (if (empty? l) 1
-            (if (empty? r) -1
-                (let [c (cmp-fn (first l) (first r))]
-                  (if (not= 0 c) c
-                      (recur (rest l) (rest r)))))))))
+    (cond
+      (every? empty? [l r]) 0
+      (empty? l) 1
+      (empty? r) -1
+      :else  (let [c (cmp-fn (first l) (first r))]
+                  (if (not= 0 c) c (recur (rest l) (rest r)))))))
 
 (defn adv-compare [l r]
-  (if (every? int? [l r])
-    (- r l)
-    (if (every? vector? [l r])
-      (adv-compare-lists l r adv-compare)
-      (if (vector? l)
-        (adv-compare l [r])
-        (adv-compare [l] r)))))
+  (cond
+    (every? int? [l r])        (- r l)
+    (every? vector? [l r])     (adv-compare-lists l r adv-compare)
+    (and (vector? l) (int? r)) (adv-compare l [r])
+    (and (int? l) (vector? r)) (adv-compare [l] r)
+    :else                      (throw (Exception. "epic fail"))))
 
 (defn -main []
-  {:part1 (->> package-pairs
+  {:part1 (->> packet-pairs
                (keep-indexed
                 (fn [i v]
                   (when (pos-int? (apply adv-compare v)) (inc i))))
                (reduce +))
-   :part2 (->> package-list
+   :part2 (->> packet-list-with-markers
                (sort-by identity adv-compare)
                (reverse)
                (keep-indexed (fn [i v] (when (is-divider? v) (inc i))))
